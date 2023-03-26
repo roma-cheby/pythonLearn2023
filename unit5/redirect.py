9beeabef74332f94251dd9a1f13f4e46aa337506
Roman <ahidovroman@mail.ru>
2023-03-27 00:51:57 +0500

    unit5 fix redirect

--- unit5/redirect.py
+++ unit5/redirect.py
@@ -1,13 +1,22 @@
+import subprocess
 import sys
+import traceback
+
 
 class Redirect():
-    def __init__(self, stdout, stderr):
-        self.strout = stdout
+    def __init__(self, stdout = sys.stdout, stderr=sys.stderr):
+        self.old_stdout = sys.stdout
+        self.old_stderr = sys.stderr
+        self.stdout = stdout
         self.stderr = stderr
     def __enter__(self):
-        sys.stdout = self.strout
-    def __exit__(self, exc_type, exc_val, exc_tb):
+        sys.stdout = self.stdout
         sys.stderr = self.stderr
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        traceback.print_exc()
+        sys.stdout = self.old_stdout
+        sys.stderr = self.old_stderr
+        return 1
 
 print('Hello stdout')
 stdout_file = open('stdout.txt', 'w')
@@ -17,7 +26,6 @@ stderr_file = open('stderr.txt', 'w')
 with Redirect(stdout=stdout_file, stderr=stderr_file):
     print('Hello stdout.txt')
     raise Exception('Hello stderr.txt')
-
 
 print('Hello stdout again')
 raise Exception('Hello stderr')
